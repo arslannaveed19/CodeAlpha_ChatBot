b472838f8016785b7ef746d3897fe92c3badf58a
arslannaveed19 <arslannaveed584@gmail.com>
2026-06-29 10:06:21 +0500

    Add files via upload

---
+++ python raf.py
@@ -0,0 +1,20 @@
+#TASK4 OF BASIC CHATBOT 
+while True:
+    user=input("~ Enter Your Question =")
+    if user=="hello":
+        print("~Hi!")
+    elif user=="how are you":
+        print("I am fine,what about you")
+    elif user=="bye":
+        print("Goodbye!")
+    else:
+        print("Sorry i don`t understand")
+
+
+
+
+
+
+
+
+
